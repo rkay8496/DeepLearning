d93157488903a18961807a235158c0c67d8c58f1
rkay8496 <rkay8496@gmail.com>
2022-12-04 21:57:28 +0900

    .....

---
+++ ActorCritic/STLTest.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import signal_tl as stl
+
+p = stl.Predicate("p") > 10
+q = stl.Predicate('q') < 5
+r = stl.Predicate('r') > 1
+
+phi1 = stl.Or([p, q, r])
+# phi2 = stl.And([stl.Not(p), q])
+# phi = stl.Always(phi)
+# phi = stl.Eventually(p)
+# phi = stl.Or(p, q)
+
+print(phi1)
+# print(phi2)
+
+# trace = {
+#     "p": stl.Signal([10, 20, 30, 5],  [0, 1, 2, 3]),
+#     'q': stl.Signal([4, 5, 6, 7], [0, 1, 2, 3])
+# }
+#
+# rob = stl.compute_robustness(phi, trace)
+# print(rob.at(0))
